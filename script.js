30dc672ba23fcdbaeb6fd2ba18b766fd3ea1ea10
RamyaLevicent <sreeramyasekar@levicent.com>
2024-10-16 19:39:24 +0530

    payment method modified and check availability added

---
+++ script.js
@@ -0,0 +1,11 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     const tabs = document.querySelectorAll('.tab-btn');
+//     tabs.forEach(tab => {
+//         tab.addEventListener('click', function () {
+//             document.querySelector('.tab-btn.active').classList.remove('active');
+//             this.classList.add('active');
+//         });
+//     });
+// });
+
+
